3bf164d3639163eeb76434c8b4d22c70789f01ea
nicoxjay <nicojay023@gmail.com>
2017-01-14 11:23:14 -0800

    eh
    bleh

--- ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/DrivingPractice.java
+++ ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/DrivingPractice.java
@@ -1,6 +1,6 @@
 
 
-        package org.firstinspires.ftc.robotcontroller.external.samples;
+             package org.firstinspires.ftc.robotcontroller.external.samples;
 
         import com.qualcomm.robotcore.eventloop.opmode.OpMode;
         import com.qualcomm.robotcore.hardware.DcMotor;
